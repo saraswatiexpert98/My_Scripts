c999e600b3a792673cd318a976385f822d20c434
saraswati <saraswati.expert98@gmail.com>
2023-08-16 01:10:03 +0530

    added read script

---
+++ scripts/reads.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "Enter your skills:"
+read SKILL
+
+echo "Your $SKILLS skill is in high demamd in the IT industry."
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo
+
+echo "Login Successfull: Welcome USER $USR,"
